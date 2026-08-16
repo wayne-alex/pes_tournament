5b8da084076a56399a4e7a025859a0786d84b144
wayne-alex <waynealex9914@gmail.com>
2026-08-16 21:47:21 +0300

    fixed name in admin to show also tournament name

--- tournaments/models.py
+++ tournaments/models.py
@@ -200,7 +200,7 @@ class Team(models.Model):
         return form
 
     def __str__(self):
-        return f"{self.tournament.name} - {self.name}"
+        return f"{self.name} - {self.tournament.name}"
 
 
 class Group(models.Model):
